0711d854371c34779f88aee7bfa902fea4ffdaba
Matthieu Lucchesi <lucchesimatthieu@gmail.com>
2023-07-05 14:40:13 +0200

    black and isort

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,17 +1,19 @@
-import tarfile
-from typing import List, Dict
-from src import get_project_root
 import os
-import matplotlib.pyplot as plt
 import pickle
+import tarfile
 from pathlib import Path
+from typing import Dict, List
+
+import matplotlib.pyplot as plt
+
+from src import get_project_root
 
 root = get_project_root()
 FOLDER_DATA = root / "data"
 
 
 ### extracting file
-def extract_data(folder=FOLDER_DATA, dest=FOLDER_DATA) -> Path  :
+def extract_data(folder=FOLDER_DATA, dest=FOLDER_DATA) -> Path:
     """extract the tar file in the folder project/data"""
     try:
         file = tarfile.open(folder / "cifar-10-python.tar.gz")
@@ -46,43 +48,48 @@ def extract_files(folder) -> Dict[str, Dict]:
 def data2img(array) -> List[List[int]]:
     """Converts the data to an array of img readable RGB"""
 
-    # Split by colors 
+    # Split by colors
     array = array.reshape(10000, 3, 32, 32)
     return [
         # Imshow wants arrays of tuple instead of three arrays
-        image.transpose(1, 2, 0) for image in array
-        ]
+        image.transpose(1, 2, 0)
+        for image in array
+    ]
+
 
 def save_images(images, folder) -> None:
     """save all image in the list images to the folder given as argument"""
-    
-    #Creates the folder if not existing
+
+    # Creates the folder if not existing
     if not os.path.exists(folder):
         os.makedirs(folder)
     print("Extracting images from folder ", folder.name, "...")
     for i, image in enumerate(images):
         plt.imsave(fname=folder / f"{i}.png", arr=image)
 
+
 def load_labels(folder, file_name) -> None:
     """Extract the labels of the images from a batch and write labels in the file **/labels.txt"""
-    with open(folder / file_name /"labels.txt", "wb") as file:
+    with open(folder / file_name / "labels.txt", "wb") as file:
         pickle.dump(extract_files(folder)[file_name][b"labels"], file=file)
 
 
 def load_batch(file_name="data_batch_1") -> None:
     """Save images and labels from a batch"""
     folder_data_extracted = extract_data()
-    save_images(data2img(extract_files(folder_data_extracted)[file_name][b"data"]), folder_data_extracted / file_name)
+    save_images(
+        data2img(extract_files(folder_data_extracted)[file_name][b"data"]),
+        folder_data_extracted / file_name,
+    )
     load_labels(folder_data_extracted, file_name)
     print("Etraction of " + file_name + " done !\n")
+
 
 def load_data(list_=["data_batch_1"]) -> None:
     """Loops over all batches and call the previous function to load a batch of images."""
     for file in list_:
-        load_batch(file.split('/')[-1])
+        load_batch(file.split("/")[-1])
 
 
 if __name__ == "__main__":
     load_data(get_batches())
-    # load_batch()
-    
