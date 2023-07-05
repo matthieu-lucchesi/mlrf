7e3907bcc0c30176df363e36ae8ad151b7e99200
Matthieu Lucchesi <lucchesimatthieu@gmail.com>
2023-07-05 15:59:09 +0200

    build_features black and isort OK

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,64 +1,57 @@
-from src import get_project_root
 import os
+import pickle
+from pathlib import Path
+
 import cv2
 import matplotlib.pyplot as plt
-import pickle
 
-IMG_FOLDER = get_project_root() / "data/"
+from src import get_project_root
+
+IMG_FOLDER = get_project_root() / "data"
 
 
-def extract(folder=IMG_FOLDER / "data_batch_1"):
+def extract(folder=IMG_FOLDER / "data_batch_1") -> None:
+    """Loops over images and extract features from image and write in a file next to it"""
     for file in os.scandir(folder):
-        if "_" not in file.name and ".png" in file.name:
+        if ".png" in file.name:
             image = cv2.imread(file.path)
-            #TRY cv2.COLOR_RGB2HLS
+            # HSV extraction
             image_reduced = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-            plt.imsave(fname=folder / (file.name.split('.')[0] + "_reduced.png"), arr=image_reduced)
-  
             # reduce colors
-            hist = cv2.calcHist([image_reduced],channels=[0,1,2],mask=None, histSize=[4,4,4], ranges=[0,180,0,256,0,256])
-            with open(folder /(file.name.split('.')[0] + "_reduced.list"),"wb") as file:
+            hist = cv2.calcHist(
+                [image_reduced],
+                channels=[0, 1, 2],
+                mask=None,
+                histSize=[4, 4, 4],
+                ranges=[0, 180, 0, 256, 0, 256],
+            )
+            with open(
+                Path(folder) / (file.name.split(".")[0] + "_hsv.list"), "wb"
+            ) as file:
                 pickle.dump(hist.flatten(), file)
 
-def extract_hog(folder=IMG_FOLDER / "data_batch_1"):
-    for file in os.scandir(folder):
-        if "_" not in file.name and ".png" in file.name:
-            image = cv2.imread(file.path)
-
-            winSize = (32,32) 
-            blockSize = (16,16) # Defaults values from here
-            blockStride = (8,8)
-            cellSize = (8,8)
+            # HOG extraction
+            winSize = (32, 32)
+            blockSize = (16, 16)  # Defaults values from here
+            blockStride = (8, 8)
+            cellSize = (8, 8)
             nbins = 9
             hog = cv2.HOGDescriptor(winSize, blockSize, blockStride, cellSize, nbins)
-            
             hist = hog.compute(image)
-
-            with open(folder /(file.name.split('.')[0] + "_reduced.list"),"wb") as file:
+            with open(
+                Path(folder) / (file.name.split(".")[0] + "_hog.list"), "wb"
+            ) as file:
                 pickle.dump(hist.flatten(), file)
 
 
-def get_X(folder):
-    res= []
-    for file_numb in range(10_000):
-        with open(folder / f"{file_numb}_reduced.png", "rb")as file:
-            img = plt.imread(fname=file)
-            sample = list(img.flatten())
+def extract_all(folder=IMG_FOLDER) -> None:
+    """Loops over the batches to get all features for all datas"""
+    for dir in os.scandir(folder):
+        if "_batch" in dir.name and dir.is_dir():
+            print(f"Extracting features for images in {dir.name}")
+            extract(dir)
+    print("Features extraction DONE")
 
-        with open(folder / f"{file_numb}_reduced.list", "rb")as file:
-            features = pickle.load(file)
-            for feat in features:
-                sample.append(feat)
-
-        res.append(sample)
-    return res
-
-def get_y(folder):
-    for file in os.scandir(folder):
-        if ".txt" in file.name:
-            return file.path
 
 if __name__ == "__main__":
-    extract(IMG_FOLDER / "data_batch_1")
-    extract(IMG_FOLDER / "data_batch_2")
-    print("Features DONE")
+    extract_all(IMG_FOLDER)
